24677aa8066d65208655cc550f5f7f1fb6f25683
Drbuddha <118621271+Drbuddha@users.noreply.github.com>
2025-07-06 21:07:31 +0545

    Add initial project files: HTML, CSS, JavaScript, and VSCode settings

---
+++ 1st day/java.js
@@ -0,0 +1,5 @@
+console.log(`Hello, World!`);
+window.alert("Hello, World!");
+console.log("I love pizza");
+document.getElementById(`FirstJScode`).textContent = "My First JavaScript Code";
+document.getElementById(`myp`).textContent = "Welcomming my self to the world of JavaScript";
