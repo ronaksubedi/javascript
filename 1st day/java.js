a059215552d12750354d8e1afd2074e626b57f92
Drbuddha <118621271+Drbuddha@users.noreply.github.com>
2025-07-07 16:38:22 +0545

    Update index.html and java.js: Add new paragraph and enhance JavaScript functionality

--- 1st day/java.js
+++ 1st day/java.js
@@ -3,3 +3,36 @@ window.alert("Hello, World!");
 console.log("I love pizza");
 document.getElementById(`FirstJScode`).textContent = "My First JavaScript Code";
 document.getElementById(`myp`).textContent = "Welcomming my self to the world of JavaScript";
+let a = 10;
+let b = 20;
+let c = a + b;
+let d = a * b;
+let e = a - b;
+let f = a / b;
+let g = a % b;
+let h = a ** b;
+console.log(`The value of a is ${a}`);
+console.log(`The value of b is ${b}`);
+console.log(`The value of c is ${c}`);
+console.log(`The value of d is ${d}`);
+console.log(`The value of e is ${e}`);
+console.log(`The value of f is ${f}`);
+console.log(`The value of g is ${g}`);
+console.log(`The value of h is ${h}`);
+console.log(`The sum of ${a} and ${b} is ${c}`);
+ 
+let x = 'Ronak';
+console.log(`My name is ${x}`);
+document.getElementById(`myname`).textContent = `My name is ${x}`;
+
+let online = true;
+if (online) {
+    console.log("I am online");
+}else {
+    console.log("I am offline");
+}
+console.log(3 + 10 * 2); 
+const i = 4 ** 3 ** 2; // Same as 4 ** (3 ** 2); evaluates to 262144
+const j = 4 / 3 / 2; // Same as (4 / 3) / 2; evaluates to 0.6666...
+console.log(`The value of a is ${i}`);
+console.log(`The value of b is ${j}`);
