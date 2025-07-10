f044bf36f9d2e32aeb90e67c6aff7e1ab2d888de
Drbuddha <118621271+Drbuddha@users.noreply.github.com>
2025-07-10 17:23:41 +0545

    Add initial HTML, CSS, and JavaScript files for random number generator

---
+++ random_number_generator/index.js
@@ -0,0 +1,17 @@
+const mybutton = document.getElementById("mybutton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
+mybutton.onclick = function (){
+    randomNumber1 = Math.floor(Math.random() * max ) + min;
+    randomNumber2 = Math.floor(Math.random() * max ) + min;
+    randomNumber3 = Math.floor(Math.random() * max ) + min;
+    label1.textContent = randomNumber1;
+    label2.textContent = randomNumber2;
+    label3.textContent = randomNumber3;
+}
